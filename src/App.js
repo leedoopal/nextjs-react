caebf2f36aac6d751eb127327c77064041ba8e1b
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-04-28 23:07:18 +0900

    outputting list of data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 
-import AllMeetupsPage from "./pages/AllMeetup";
+import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
 import MainNavigation from "./components/layout/MainNavigation";
